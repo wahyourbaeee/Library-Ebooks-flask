74271767e77aae947ed1acc7151806fa6a88d785
wwahyutirta@gmail.com <wwahyutirta@gmail.com>
2025-08-23 08:46:38 +0700

    flask , html with render_template

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # main
 
-from flask import Flask
+from flask import Flask, render_template , request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main ():
+    return render_template("index.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
